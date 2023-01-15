e44b0cd0ab3fcc1a21b7f71174ca92ab3abed0df
rishabh soni <sonirishabh2207@gmail.com>
2023-01-15 16:03:02 +0530

    img updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ const Card = ({ item }) => {
   return (
     <div>
       <div
-        className="object-fit h-[40vh] rounded-sm text-white relative bg-gray-900/30 overflow-hidden"
+        className="object-cover h-[40vh] rounded-sm text-white relative bg-gray-900/30 overflow-hidden"
         key={item.id}
       >
-        <img src={item.url} alt="img" className="w-full h-full" />
+        <img src={item.url} alt="img" className="w-full h-full object-cover" />
         <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
           <h1 className="absolute bottom-2 left-3 text-2xl font-bold">
             {item.title}
